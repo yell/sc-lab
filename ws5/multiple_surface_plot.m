b197ba2bcdb5e52b5a493b59936feef296b74e43
monsta <yell.bondarenko@gmail.com>
2017-01-20 01:01:42 +0100

    setup main file with mock for implicit Euler

--- ws5/multiple_surface_plot.m
+++ ws5/multiple_surface_plot.m
@@ -1,7 +1,7 @@
 function multiple_surface_plot(T_collection, title_str, title_strs)
 %{
-Plot the whole collection of matrices each evenly spaced in [0; 1] x [0; 1].
-
+Plot the whole collection of matrices each 
+evenly spaced in [0; 1] x [0; 1].
 
 Parameters
 ----------
@@ -13,10 +13,10 @@ title_strs : (M, N) cell-array of strings
 	Corresponding titles for subplots.
 %}
 fig = figure;
-set(fig, 'position', [10 10 1400 720]);
+[M N] = size(T_collection);
+set(fig, 'position', [10, 10, (280 * N + 180), 800]);
 set(fig, 'visible', 'on');
 
-[M N] = size(T_collection);
 colormap jet;
 grid on;
 
@@ -27,9 +27,9 @@ for p = 1:numel(1:M)
 		
 		% make subplots wider and reduce margins
 		pos = get(g, 'position');
-		pos(1) = pos(1) - 0.09 + 0.022 * (q - 1);
+		pos(1) = pos(1) - 0.08 + 0.022 * (q - 1);
 		pos(2) = pos(2) - 0.03 - 0.01 * (p - 1);
-		pos(3) = pos(3) * 1.25; 
+		pos(3) = pos(3) * 1.22; 
 		set(g, 'position', pos);
 		
 		[m n] = size(T_collection{p, q});
--- ws5/worksheet5.m
+++ ws5/worksheet5.m
@@ -2,33 +2,36 @@ clc
 clear
 
 
-% spatial resolutions
-N_xs = 2 .^ (2:5) - 1; % 3, 7, ..., 31
-
-% solving methods
-step_methods = { @explicit_euler_step };
-step_methods_strs = {'Explicit Euler', 'Implicit Euler'};
-
-% temporal step sizes
-dts_collection = {
-	2 .^ (-6:-1:-12), % 1/64, ..., 1/4096
-	[2 ^ (-6)]
-};
-
-% whether to plot solutions
+% some control variables
 plot_solutions = true;
 save_solutions = true;
+explicit_euler = false;
+implicit_euler = true;
+
+% spatial resolutions
+N_xs = 2 .^ (2:5) - 1; % 3, 7, ..., 31
 
 % at which points in time to plot
 plot_times = (1:4)/8;
 plot_dts = diff([0, plot_times]);
 
 
-% main loop
-for i = 1:numel(step_methods)
-	dts = dts_collection{i};
+fprintf('WARNING: do NOT close figures very quickly, \n         they need to be saved (at least that with subplots)!\n\n');
 
-	% here all solutions are stored
+% 
+%
+% Explicit Euler
+
+if explicit_euler
+
+	step_method = @explicit_euler_step;
+	step_method_str = 'Explicit Euler';
+	fprintf('Computing and plotting %s ...\n\n', step_method_str);
+
+	dts = 2 .^ (-6:-1:-12); % 1/64, ..., 1/4096
+
+	% here all solutions are stored 
+	% (and corresponding titles for subplots)
 	T_collection = cell(numel(N_xs), numel(dts));
 	title_strs = cell(numel(N_xs), numel(dts));
 
@@ -42,6 +45,7 @@ for i = 1:numel(step_methods)
 		end
 	end
 
+	% main loop
 	for l = 1:numel(plot_dts)
 		% for all grid sizes and time steps
 		% compute to arrive to corresponding `plot_times`
@@ -49,51 +53,110 @@ for i = 1:numel(step_methods)
 			for k = 1:numel(dts)
 				for o = 1:numel(1:int16(plot_dts(l)/dts(k)))
 					T_prev = T_collection{j, k};
-					T_collection{j, k} = step_methods{i}(T_prev, dts(k));	
+					T_collection{j, k} = step_method(T_prev, dts(k));	
 				end
+			end
+			% save separate plots as soon as available
+			if plot_solutions && save_solutions
+				title_str = strcat(step_method_str, ...
+					               ', N_x=', num2str(N_xs(j)), ...
+					               ', dt=2^{-', num2str(k + 5), '}', ...
+					               ', 8t=', num2str(plot_times(l) * 8));
+				surface_plot(T_collection{j, k}, title_str);
 			end
 		end
 
-		
+		% display and save windows with subplots
 		if plot_solutions
-			% plot together
-			title_str = strcat(step_methods_strs{i}, ...
+			title_str = strcat(step_method_str, ...
 				               ', 8t=', num2str(plot_times(l) * 8));
 			multiple_surface_plot(T_collection, title_str, title_strs);
+		end
+	end
 
-			% ... and separately
-			if save_solutions
-				for j = 1:numel(N_xs)
-					for k = 1:numel(dts)
-						title_str = strcat(step_methods_strs{i}, ...
-							               ', N_x=', num2str(N_xs(j)), ...
-							               ', dt=2^{-', num2str(k + 5), '}', ...
-							               ', 8t=', num2str(plot_times(l) * 8));
-						surface_plot(T_collection{j, k}, title_str);
-					end
+	% print stability tabular
+	fprintf(' Explicit Euler stability\n');
+	fprintf('\n N_x = N_y \\ dt ');
+	for k = 1:numel(dts)
+		fprintf('| 1/%4d ', int16(1/dts(k)));
+	end
+	fprintf('\n');
+	fprintf(repmat('-', 1, 79));
+	for j = 1:numel(N_xs)
+		fprintf('\n       %2d       ', N_xs(j));
+		for k = 1:numel(dts)
+			if 2 * dts(k) * (1 + N_xs(j)) ^ 2 < 1
+				fprintf('|    +   ');
+			else
+				fprintf('|    -   ');
+			end
+		end
+	end
+	fprintf('\n\n')
+
+end % explicit_euler
+
+
+
+%
+%
+% Implicit Euler
+
+if implicit_euler
+
+	step_method = @explicit_euler_step;
+	step_method_str = 'Implicit Euler';
+	fprintf('\nComputing and plotting %s ...\n\n', step_method_str);
+
+	dt = 1/64;
+
+	% here all solutions are stored 
+	% (and corresponding titles for subplots)
+	T_collection = cell(numel(N_xs), numel(plot_times));
+	title_strs = cell(numel(N_xs), numel(plot_times));
+
+	% initialize all matrices
+	for j = 1:numel(N_xs)
+		N_x = N_xs(j);
+		N_y = N_x;
+		T_collection{j, 1} = make_initial([N_x N_y]);
+		for l = 1:numel(plot_times)
+			title_strs{j, l} = strcat('N_x=', num2str(N_xs(j)), ...
+				                      ', 8t=', num2str(plot_times(l) * 8));
+		end
+	end
+
+	% main loop
+	for l = 1:numel(plot_dts)
+		% for all grid sizes compute
+		% to arrive to corresponding `plot_times`
+		for j = 1:numel(N_xs)
+			for o = 1:numel(1:int16(plot_dts(l)/dt))
+				T_prev = T_collection{j, l};
+				T_collection{j, l} = step_method(T_prev, dt);	
+			end
+			% copy all solutions to the next
+			% for further computations
+			if l < numel(plot_dts)
+				T_collection{j, l + 1} = T_collection{j, l};
+			end
+			% save separate plots as soon as available
+			if plot_solutions && save_solutions
+				if (j == 1) % TODO: remove
+					title_str = strcat(step_method_str, ...
+						               ', N_x=', num2str(N_xs(j)), ...
+						               ', dt=2^{-6}, 8t=', num2str(plot_times(l) * 8));
+					surface_plot(T_collection{j, l}, title_str);
 				end
 			end
 		end
 	end
-end
 
-
-% print stability tabular for Explicit Euler (i = 1)
-fprintf('\n Explicit Euler stability\n');
-fprintf('\n N_x = N_y \\ dt ');
-for k = 1:numel(dts_collection{1})
-	fprintf('| 1/%4d ', int16(1/dts_collection{1}(k)));
-end
-fprintf('\n');
-fprintf(repmat('-', 1, 79));
-for j = 1:numel(N_xs)
-	fprintf('\n       %2d       ', N_xs(j));
-	for k = 1:numel(dts_collection{1})
-		if 2 * dts_collection{1}(k) * (1 + N_xs(j)) ^ 2 < 1
-			fprintf('|    +   ');
-		else
-			fprintf('|    -   ');
-		end
+	% display and save windows with subplots
+	if plot_solutions
+		title_str = strcat(step_method_str, ', dt=2^{-6}');
+		multiple_surface_plot(T_collection, title_str, title_strs);
 	end
-end
-fprintf('\n')
+
+
+end % implicit_euler
